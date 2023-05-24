7b46b3ecd1b9700d032a81e1a03fd718130e0a16
madnecat <mad.necat@gmail.com>
2023-05-24 07:02:02 +0100

    dispose db connection after each message

--- src/server.ts
+++ src/server.ts
@@ -22,20 +22,19 @@ async function SetUpTwitchListener() {
             username: 'madnecatbot',
             password: `oauth:${process.env.TWITCH_OAUTH_TOKEN}`
         },
-        connection: {port: process.env.PORT || 443},
+        connection: { port: process.env.PORT || 443 },
         channels: ['madnecat']
     });
     client.connect();
-    connectToDatabase().then(async () => {
-        client.on('message', async (channel: string, tags: any, message: string, self: any) => {
-            console.log(`${tags['display-name']}: ${message}`);
-            await MessageManagement(message, tags, client, channel);
-            if (message.startsWith('!')) {
-                await CommandManagement(message, tags, client, channel);
-            }
-        });
-        console.log('app started successfully');
+    client.on('message', async (channel: string, tags: any, message: string, self: any) => {
+        console.log(`${tags['display-name']}: ${message}`);
+        await connectToDatabase();
+        await MessageManagement(message, tags, client, channel);
+        if (message.startsWith('!')) {
+            await CommandManagement(message, tags, client, channel);
+        }
     });
+    console.log('app started successfully');
 }
 
 async function CommandManagement(message: string, tags: any, client: any, channel: string) {
